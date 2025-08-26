4887365351c5ab1b1e47586ef7f936af3aaebf8d
Ashutosh ojha <ashutoshojha18@gmail.com>
2025-08-26 16:30:45 +0530

    Fix routing for GitHub Pages using HashRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { BlogProvider } from './context/BlogContext';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -15,7 +15,7 @@ import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
 import BlogPage from './pages/BlogPage';
 import BlogDetail from './pages/BlogDetail';
-import BlogCMS from './components/BlogCMS';
+// import BlogCMS from './components/BlogCMS';
 import './styles/index.css';
 
 function App() {
